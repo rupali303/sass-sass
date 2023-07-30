c9ea325c191fb69d15777ceaae5f175beab23035
Rupali Patil <121420287+rupali303@users.noreply.github.com>
2023-07-30 20:24:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let cl = console.log;
+
+const navigation = document.getElementById("navigation");
+const naviToggle = document.getElementById("naviToggle");
+
+
+
+
+
+
+
+
+naviToggle.addEventListener("click", ()=>{
+    navigation.classList.toggle('active')
+})
